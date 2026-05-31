50db6a12c784b4de4770cd0628671fc6a15cb3fd
s26126 <s26126@pjwstk.edu.pl>
2026-05-31 22:53:37 +0200

    Initial commit: szkielet API szpitala z modelem EF Core (database-first)

---
+++ APBD-s26126-Hospital-dbfirst/Models/BedType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APBD_s26126_Hospital_dbfirst.Models;
+
+public partial class BedType
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public virtual ICollection<Bed> Beds { get; set; } = new List<Bed>();
+}
